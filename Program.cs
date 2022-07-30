ded5f2a25236f09baa029114aa7bab2c411e68ac
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-07-30 17:29:51 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace App_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 18;
+            var suma = num1 + num2;
+            int edad = 18;
+            string name = "Patrick";
+            Console.WriteLine(suma);
+            Console.WriteLine("Su nombre es: {1} y su edad es: {0}", edad, name);
+        }
+    }
+}
